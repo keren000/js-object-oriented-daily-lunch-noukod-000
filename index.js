4d7ee7a2c4f40ec83cddcfbd169acd27896218a5
Keren-Happuc Fleurimond <keren@noukod.com>
2019-02-27 17:26:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
-let neighborhoodId = 0 
+let neighborhoodId = 0
 //class of neighborhood
 class Neighborhood {
 	constructor(name) {
