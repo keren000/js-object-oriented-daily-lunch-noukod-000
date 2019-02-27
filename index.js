d85a66b3882f62a0f517f6fb0a5953107febd1ed
Keren-Happuc Fleurimond <keren@noukod.com>
2019-02-27 17:27:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
 let neighborhoodId = 0
+
 class Neighborhood {
 	constructor(name) {
     this.id = ++neighborhoodId;
@@ -80,8 +81,8 @@ let deliveryId = 0
 class Delivery {
 	constructor(mealId=, neighborhoodId, customerId={}) {
 		this.mealId = mealId;
-		this.customerId = customerId;
 		this.neighborhoodId = neighborhoodId;
+		this.customerId = customerId;
 		this.id = ++deliveryId;
 		store.deliveries.push(this);
 	}
